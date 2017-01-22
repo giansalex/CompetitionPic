3a915f5e263211ed4cc5d5936c8782cbee0af5e4
Giancarlos Salas <giansalex@gmail.com>
2017-01-22 12:56:13 -0500

    LCD420 + Change Design + Completed

--- main.h
+++ main.h
@@ -7,21 +7,3 @@
 #FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
 
 #use delay(clock=4M)
-
-#define LCD_ENABLE_PIN PIN_C7
-#define LCD_RS_PIN PIN_C5
-#define LCD_RW_PIN PIN_C6
-#define LCD_Data4  PIN_D4
-#define LCD_Data5  PIN_D5
-#define LCD_Data6  PIN_D6
-#define LCD_Data7  PIN_D7
-
-
-#define LCD_DB4   PIN_B4 
-#define LCD_DB5   PIN_B5 
-#define LCD_DB6   PIN_B6 
-#define LCD_DB7   PIN_B7 
-
-#define LCD_RS    PIN_C5 
-#define LCD_RW    PIN_C6 
-#define LCD_E     PIN_C7 
